46695942b60c134a47ad4e8cf1430cc4c613e09c
Jed Rendo Magracia <jedmagracia@gmail.com>
2022-04-23 13:21:38 -0400

    Added database and script

---
+++ database.py
@@ -0,0 +1,22 @@
+"""
+Using Python to create a SQLite3 Database
+"""
+
+import sqlite3
+
+DATABSE_NAME = "database.db"
+conn = sqlite3.connect(DATABSE_NAME)
+cur = conn.cursor()
+
+# Date format: '2007-01-01 10:00:00'
+cur.execute(
+    """
+    CREATE TABLE IF NOT EXISTS Transactions (
+        TransactionID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        Datetime DATETIME NOT NULL,
+        Amount DOUBLE NOT NULL,
+        MerchantName TEXT NOT NULL,
+        Type TEXT NOT NULL
+    )
+    """
+)
