9de1c179acb64adb210a6d5524c6e6868f07faa6
Jed Rendo Magracia <jedmagracia@gmail.com>
2022-05-05 00:39:07 -0400

    Added setters, getters, constructor, equals, toString, and hashCode for TransactionDatabase.java

--- app/src/main/java/com/example/budgetingapp/model/TransactionDatabase.java
+++ app/src/main/java/com/example/budgetingapp/model/TransactionDatabase.java
@@ -1,5 +1,40 @@
 package com.example.budgetingapp.model;
 
+import java.util.Objects;
+
 public class TransactionDatabase {
     private String url;
+
+    // TODO: Connect the program to the database in the constructor
+    public TransactionDatabase(String url) {
+        this.url = url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String toString() {
+        return "TransactionDatabase{" +
+                "url='" + url + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TransactionDatabase that = (TransactionDatabase) o;
+        return url.equals(that.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url);
+    }
 }
