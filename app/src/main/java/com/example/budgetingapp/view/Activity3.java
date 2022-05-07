0c53b57e42905108cdccbedcdbf75ad7e9549141
talikeb <talikeb20@gmail.com>
2022-05-07 15:54:48 -0400

    updated activity 3 functionality (number buttons actually work)

--- app/src/main/java/com/example/budgetingapp/view/Activity3.java
+++ app/src/main/java/com/example/budgetingapp/view/Activity3.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.budgetingapp.R;
 
@@ -17,6 +19,7 @@ public class Activity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity3);
+        // Log.d("current amount", ((TextView) findViewById(R.id.moneyAmount)).getText().toString());
         getSupportActionBar().hide();
         Button removeButton = (Button) findViewById(R.id.doneButton);
         removeButton.setOnClickListener(new View.OnClickListener(){
@@ -32,4 +35,30 @@ public class Activity3 extends AppCompatActivity {
             }
         });
     }
+
+    // TODO: (@Front-End) Revise method.
+    public void addNum(View v) {
+        String amount = ((TextView) findViewById(R.id.moneyAmount)).getText().toString();
+        // double amount_double = Double.parseDouble(amount.substring(1));
+        String num = ((Button) v).getText().toString();
+        // Log.d("number value", num);
+
+        if(amount.charAt(1) == '0') {
+            String newStr = "$" +
+                    amount.charAt(3) +
+                    '.' +
+                    amount.charAt(4) +
+                    num;
+            ((TextView) findViewById(R.id.moneyAmount)).setText(newStr);
+        } else {
+            String newStr = '$' +
+                    amount.substring(1, amount.indexOf('.')) +
+                    amount.charAt(amount.indexOf('.') + 1) +
+                    '.' +
+                    amount.charAt(amount.length() - 1) +
+                    num;
+            ((TextView) findViewById(R.id.moneyAmount)).setText(newStr);
+        }
+    }
+
 }
