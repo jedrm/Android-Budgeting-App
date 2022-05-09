15b6637d99d9c6dd2cefdb47ce6eeeee7dd73d13
talikeb <talikeb20@gmail.com>
2022-05-08 20:33:28 -0400

    updated activity 3 functionality (numbers can be removed)

--- app/src/main/java/com/example/budgetingapp/view/Activity3.java
+++ app/src/main/java/com/example/budgetingapp/view/Activity3.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,7 +21,7 @@ public class Activity3 extends AppCompatActivity {
         setContentView(R.layout.activity3);
         // Log.d("current amount", ((TextView) findViewById(R.id.moneyAmount)).getText().toString());
         getSupportActionBar().hide();
-        Button removeButton = (Button) findViewById(R.id.doneButton);
+        Button removeButton = (Button) findViewById(R.id.removeButton);
         removeButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 startActivity(new Intent(Activity3.this, Activity4.class));
@@ -38,37 +37,36 @@ public class Activity3 extends AppCompatActivity {
     }
 
     public void addNum(View v) {
+
         String amount = ((TextView) findViewById(R.id.moneyAmount)).getText().toString();
-        // double amount_double = Double.parseDouble(amount.substring(1));
         String num = ((Button) v).getText().toString();
-        // Log.d("number value", num);
-
-        /*
-        if(amount.charAt(1) == '0') {
-            String newStr = "$" +
-                    amount.charAt(3) +
-                    '.' +
-                    amount.charAt(4) +
-                    num;
-            ((TextView) findViewById(R.id.moneyAmount)).setText(newStr);
-        } else {
-            String newStr = '$' +
-                    amount.substring(1, amount.indexOf('.')) +
-                    amount.charAt(amount.indexOf('.') + 1) +
-                    '.' +
-                    amount.charAt(amount.length() - 1) +
-                    num;
-            ((TextView) findViewById(R.id.moneyAmount)).setText(newStr);
-        }
-        */
-
         StringBuilder sb = new StringBuilder();
+
         sb.append('$');
         if(amount.charAt(1) != '0') sb.append(amount.substring(1, amount.indexOf('.')));
         sb.append(amount.charAt(amount.indexOf('.') + 1));
         sb.append('.');
         sb.append(amount.charAt(amount.length() - 1));
         sb.append(num);
+        ((TextView) findViewById(R.id.moneyAmount)).setText(sb.toString());
+
+    }
+
+    public void removeNum(View v) {
+
+        String amount = ((TextView) findViewById(R.id.moneyAmount)).getText().toString();
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb_reverse = new StringBuilder();
+
+        for(int i = amount.length() - 1; i >= 0; i--) sb_reverse.append(amount.charAt(i));
+
+        sb.append(sb_reverse.charAt(1));
+        sb.append(sb_reverse.charAt(3));
+        sb.append('.');
+        if(sb_reverse.charAt(4) == '$' && sb_reverse.charAt(3) != 0) sb.append("0$");
+        else sb.append(sb_reverse.substring(4));
+
+        sb.reverse();
         ((TextView) findViewById(R.id.moneyAmount)).setText(sb.toString());
 
     }
--- app/src/main/java/com/example/budgetingapp/view/Activity4.java
+++ app/src/main/java/com/example/budgetingapp/view/Activity4.java
@@ -2,7 +2,6 @@ package com.example.budgetingapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
