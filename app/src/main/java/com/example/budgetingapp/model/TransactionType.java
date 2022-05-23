498c14cd5d4bab2cb77efe645526dc903592b5d2
Jed Rendo Magracia <jedmagracia@gmail.com>
2022-05-22 20:46:35 -0400

    Removed income from transaction type

--- app/src/main/java/com/example/budgetingapp/model/TransactionType.java
+++ app/src/main/java/com/example/budgetingapp/model/TransactionType.java
@@ -4,7 +4,6 @@ package com.example.budgetingapp.model;
 Enum for the different categories of transactions
  */
 public enum TransactionType {
-    Income,
     Entertainment,
     Shopping,
     Health,
